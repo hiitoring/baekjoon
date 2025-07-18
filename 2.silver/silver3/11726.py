3f412441835cb9a33dde66b29a53abbf9b258166
hiitoring <gmlcjf132@naver.com>
2025-07-18 16:14:37 +0900

    2025-07-18

--- 2.silver/silver3/11726.py
+++ 2.silver/silver3/11726.py
@@ -1,9 +1,11 @@
 n = int(input())
 tile = [0] * (n+1)
 tile[1] = 1
+j = 1
 for i in range(2,n+1):
     if i == 2:
-        tile[i] = 2
+        tile[i] = 3
     else:
-        tile[i] = tile[i-1] + tile[i-2]
+        tile[i] = tile[i-1] * 2 + j
+    j *= -1
 print(tile[n]%10007)
