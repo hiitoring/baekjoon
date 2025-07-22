172bdac229c1b8601834fb2f8785296d991ee876
hiitoring <gmlcjf132@naver.com>
2025-07-22 16:42:37 +0900

    2025-07-22

---
+++ 2.silver/silver1/11052.py
@@ -0,0 +1,11 @@
+t = int(input())
+card = [0] + list(map(int,input().split()))
+value = [0] * (t+1)
+value[1] = card[1]
+for i in range(2,t+1):
+    value[i] = card[i]
+    for j in range(i-1,-1,-1):
+        vigo = value[i-j] + card[j]
+        if value[i] < vigo:
+            value[i] = vigo
+print(value[t])
