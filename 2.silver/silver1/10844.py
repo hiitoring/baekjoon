0a8a3f0d18830ff3aa777b8777ceb35e3efc9972
hiitoring <gmlcjf132@naver.com>
2025-07-28 16:43:34 +0900

    2025-07-28

---
+++ 2.silver/silver1/10844.py
@@ -0,0 +1,15 @@
+n = int(input())
+answer = [1] * 10
+answer[0] = 0
+support = [0] * 10
+for i in range(n-1):
+    for j in range(10):
+        if j == 0:
+            support[j] = answer[j+1]
+        elif j == 9:
+            support[j] = answer[j-1]
+        else:
+            support[j] = answer[j-1] + answer[j+1]
+    for k in range(10):
+        answer[k] = support[k]
+print(sum(answer))
